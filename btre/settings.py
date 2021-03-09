94934bd2e3238b9e5ea9edafcdf02c1a3c6e1d37
sofialazrak <60613958+sofialazrak@users.noreply.github.com>
2021-03-09 23:57:17 +0100

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -147,6 +147,6 @@ MESSAGE_TAGS = {
 # Email config
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sofia.lazrak@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sol@med66'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
